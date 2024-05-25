d7d85de6975817f78fbd11a19e60ca74f18523cf
lukas-laas <111971517+lukas-laas@users.noreply.github.com>
2024-05-25 17:30:01 +0200

    start working on page skeleton

---
+++ src/components/addressList/addressList.tsx
@@ -0,0 +1,12 @@
+export default function header() {
+  return (
+    <div>
+      <h2>This is the addresses:</h2>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
+}
