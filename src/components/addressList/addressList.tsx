62058f6be7c590b44c5cf6b23249a24d94ac474e
lukas-laas <lukas.s.laas@gmail.com>
2024-05-29 22:41:32 +0200

    feature: filter by name

--- src/components/addressList/addressList.tsx
+++ src/components/addressList/addressList.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import Image from "next/image";
+import { useMemo, useRef, useState } from "react";
 
 export default function AddressList({
   addresses,
@@ -16,7 +17,27 @@ export default function AddressList({
     [addresses]
   );
 
+  const inputRef = useRef<HTMLInputElement>();
   const [order, setOrder] = useState(ascending);
+
+  function search() {
+    const filteredArray: object[] = [];
+    ascending.forEach((element) => {
+      if (inputRef.current && inputRef.current.value !== "") {
+        if (
+          element.Name.toLowerCase().includes(
+            inputRef.current.value.toLowerCase()
+          )
+        ) {
+          filteredArray.push(element);
+        }
+
+        setOrder(filteredArray);
+        return;
+      } else setOrder(ascending);
+      return;
+    });
+  }
 
   return (
     <>
@@ -28,20 +49,26 @@ export default function AddressList({
           Reorder
         </button>
         <input
+          ref={inputRef}
           type="text"
           placeholder="Search"
-          className="rounded-md border"
-          onChange={order.find((element) => order.includes(element))}
+          className="rounded-md border text-black"
+          onChange={() => search()}
         />
       </div>
       <ol className="w-full">
         {order.map((x) => {
           return (
-            <li key={x.id} className="border p-1 rounded-md bg-gray-800 my-1">
-              <p>
-                {x.Name}, {x.street}:{x.houseNumber} {x.postalCode}
-              </p>
-              <p>phone: {x.telephone}</p>
+            <li
+              key={x.id}
+              className="flex flex-row border p-1 rounded-md bg-gray-800 my-1"
+            >
+              <div>
+                <p>
+                  {x.Name}, {x.street}:{x.houseNumber} {x.postalCode}
+                </p>
+                <p>phone: {x.telephone}</p>
+              </div>
             </li>
           );
         })}
